0b1e5e5f2a5f0c3758cd93ebb393060dab0f9ffd
koh-suzuki <k.s_coward_0629@icloud.com>
2019-06-26 02:25:12 +0000

    8. 4 全てのユーザーを表示するページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
+  def index
+    @users = User.all
+  end
+
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -22,11 +25,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       redirect_to @user
     else
@@ -58,7 +59,6 @@ class UsersController < ApplicationController
     
     # アクセスしたユーザーが現在ログインしているユーザーか確認します。
     def correct_user
-      @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
 end
